e472d0c60e8f9887be6b095822241148a9a37e8f
dayCod <alayarahman07@gmail.com>
2023-08-23 11:20:30 +0700

    Add: Database Migration

---
+++ app/Models/UserWalletMutation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserWalletMutation extends Model
+{
+    use HasFactory;
+}
--- database/migrations/2023_08_23_041427_create_leads_table.php
+++ database/migrations/2023_08_23_041427_create_leads_table.php
@@ -13,17 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('leads', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('member_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('email');
             $table->string('phone_number');
-            $table->string('google_id')->nullable();
-            $table->boolean('is_online')->default(false);
-            $table->enum('role', ['admin', 'customer', 'member']);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -35,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('leads');
     }
 };
